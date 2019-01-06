c0a69268ac9c078c98c9c037ef9a0e5a0b451730
Sean Humeniuk <seanhumeniuk@gmail.com>
2019-01-06 18:24:05 -0500

    Remove default test

--- src/lib.rs
+++ src/lib.rs
@@ -11,11 +11,3 @@ pub use self::vector4::*;
 pub use self::matrix4::*;
 pub use self::quaternion::*;
 pub use self::ray::*;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
