9ecc689fa5356e5792e225181c439e200b57d9a2
Sean Humeniuk <seanhumeniuk@gmail.com>
2019-01-15 18:59:24 -0500

    Rename vector2.rs to vector2i.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
-pub mod vector2;
+pub mod vector2i;
 pub mod vector3;
 pub mod vector4;
 pub mod matrix4;
 pub mod quaternion;
 pub mod ray;
 
-pub use self::vector2::*;
+pub use self::vector2i::*;
 pub use self::vector3::*;
 pub use self::vector4::*;
 pub use self::matrix4::*;
